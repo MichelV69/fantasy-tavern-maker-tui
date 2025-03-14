945dbb5b9aed0c67ae3a748ea85f8ac6e3c07d90
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2025-03-13 22:25:04 -0300

    basic, but ugly, implementation of Cursive TUI

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@
 #![allow(unused_mut)]
 #![allow(non_snake_case)]
 
+use cursive::views::Dialog;
+use cursive::views::TextView;
+use cursive::Cursive;
 use tracing::info;
+use cursive;
 
 // --- my stuff ---
 mod dice_bag;
@@ -17,6 +21,22 @@ use crate::tavern::*;
 
 // --- local cli code
 fn main() -> () {
-    print!("{}", get_pbhouse());
+  let mut siv = cursive::default();
+
+  siv.add_layer(Dialog::text("Welcome to fantasy-tavern-maker-tui")
+      .title("fantasy-tavern-maker-tui")
+      .button("New",|s| get_new_pbhouse(s))
+      .button("Finish", |s| s.quit()));
+
+ siv.run()
 }
+
+fn get_new_pbhouse(s: &mut Cursive) -> () {
+    s.pop_layer();
+    s.add_layer(Dialog::text(get_pbhouse())
+        .title("Results")
+        .button("Another",|s| get_new_pbhouse(s))
+        .button("Finish", |s| s.quit()));
+}
+
 // --- eof ---
