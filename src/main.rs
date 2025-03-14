1ee253b15f9e7e727014f7a6acb691ed47ec8318
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2025-03-14 16:26:22 -0300

    Now writing files to disk

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,12 @@ use cursive::view::Scrollable;
 use cursive::views::Dialog;
 use cursive::views::LinearLayout;
 use cursive::views::TextView;
+use dirs::download_dir;
 use tracing::info;
+
+use std::fmt::format;
+use std::fs::File;
+use std::io::prelude::*;
 
 // --- my stuff ---
 mod dice_bag;
@@ -91,14 +96,40 @@ fn get_new_pbhouse(s: &mut Cursive, app: App) -> () {
                     ),
             )
             .button("Quit", |s| s.quit())
-            .button("Save to file",  move |s| save_pbhouse_to_file(s, pbh.clone(), app1.clone()))
-            .button("Roll another",  move |s| get_new_pbhouse(s, app2.clone()))
+            .button("Save to file", move |s| {
+                save_pbhouse_to_file(s, pbh.clone(), app1.clone())
+            })
+            .button("Roll another", move |s| get_new_pbhouse(s, app2.clone()))
             .h_align(cursive::align::HAlign::Center),
     );
 }
 
-fn save_pbhouse_to_file(s: &mut Cursive, pbh:PBHouse, app: App ) {
+fn save_pbhouse_to_file(s: &mut Cursive, pbh: PBHouse, app: App) {
     s.pop_layer();
+
+    let path_name = match download_dir() {
+        Some(value) => value,
+        None => panic!("Problem determining user download dir!"),
+    };
+
+    let fname = format!(
+        "{}/{}-{}.md",
+        path_name.display(),
+        app.name,
+        pbh.name.to_lowercase().replace(" ", "_")
+    );
+    let file_open_result = File::create(fname);
+
+    let mut file_handle = match file_open_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {error:?}"),
+    };
+
+    let file_write_result = write!(file_handle, "{}", format!("\n \n {} \n \n", pbh));
+    let work_result = match file_write_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem writing to the file: {error:?}"),
+    };
 
     s.add_layer(
         Dialog::text(&format!("{} - saving {} to disk", &app.name, pbh.name))
