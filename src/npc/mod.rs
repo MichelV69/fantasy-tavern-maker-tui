e91eab4aaf6fa4a2459635feaf41af9afe19ab7f
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2025-03-14 23:37:17 -0300

    moving testing to its own file

--- src/npc/mod.rs
+++ src/npc/mod.rs
@@ -49,25 +49,5 @@ pub mod Build {
         Male,
     }
 } //pub mod NPC
-
-// ---- start of tests ----
 #[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::npc::Build::*;
-    use tracing::{Level, event};
-
-    #[test]
-    fn profile_new() {
-        let test_pilot: Profile = Profile::new();
-
-        debug_assert!(test_pilot.npc_type == NpcTypeCode::Patron);
-        debug_assert!(test_pilot.gender == GenderCode::Androgynous);
-        debug_assert!(test_pilot.public_name == "New NPC");
-        debug_assert!(test_pilot.task_description == "Realm's Most Interesting Person");
-        debug_assert!(test_pilot.species == SpeciesCode::Dragonborn);
-        debug_assert!(test_pilot.height_desc  == "about average");
-
-    }
-} // mod tests
-// ---- end of file ----
+mod tests;
---
+++ src/npc/tests.rs
@@ -0,0 +1,21 @@
+// ---- start of tests ----
+
+mod suite {
+    use super::*;
+    use crate::npc::Build::*;
+    use tracing::{Level, event};
+
+    #[test]
+    fn profile_new() {
+        let test_pilot: Profile = Profile::new();
+
+        debug_assert!(test_pilot.npc_type == NpcTypeCode::Patron);
+        debug_assert!(test_pilot.gender == GenderCode::Androgynous);
+        debug_assert!(test_pilot.public_name == "New NPC");
+        debug_assert!(test_pilot.task_description == "Realm's Most Interesting Person");
+        debug_assert!(test_pilot.species == SpeciesCode::Dragonborn);
+        debug_assert!(test_pilot.height_desc  == "about average");
+
+    }
+} // mod tests
+// ---- end of file ----
