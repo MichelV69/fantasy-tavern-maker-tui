78d430862e90b5a9330a45cae219944f944667c6
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2025-03-14 23:25:29 -0300

    continuing on building out NPC profile, with test coverage

--- src/npc/mod.rs
+++ src/npc/mod.rs
@@ -7,6 +7,8 @@ pub mod Build {
         pub gender: GenderCode,
         pub public_name: String,
         pub task_description: String,
+        pub species: SpeciesCode,
+        pub height_desc: String,
     }
 
     impl Profile {
@@ -16,8 +18,21 @@ pub mod Build {
                 gender: GenderCode::Androgynous,
                 public_name: "New NPC".into(),
                 task_description: "Realm's Most Interesting Person".into(),
+                species: SpeciesCode::Dragonborn,
+                height_desc: "about average".into()
             }
         }
+    }
+
+    #[derive(PartialEq)]
+    pub enum SpeciesCode {
+        Dragonborn,
+        Dwarf,
+        Elf,
+        Gnome,
+        Halfling,
+        Human,
+        Tiefling,
     }
 
     #[derive(PartialEq)]
@@ -50,6 +65,9 @@ mod tests {
         debug_assert!(test_pilot.gender == GenderCode::Androgynous);
         debug_assert!(test_pilot.public_name == "New NPC");
         debug_assert!(test_pilot.task_description == "Realm's Most Interesting Person");
+        debug_assert!(test_pilot.species == SpeciesCode::Dragonborn);
+        debug_assert!(test_pilot.height_desc  == "about average");
+
     }
 } // mod tests
 // ---- end of file ----
