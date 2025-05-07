632ca85f99ffc4862a1bb97f6131e5676e443851
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2025-05-07 22:30:56 +0000

    starting to build out tests / use cases now that initial code-sketch of the mod.rs is done.
    TDD : RED

--- src/narrative_time_manager/tests.rs
+++ src/narrative_time_manager/tests.rs
@@ -1,7 +1,12 @@
 mod suite { 
     #[test]
-    fn demo() {
-        debug_assert_eq(1,1);
+    fn number_of_timeslots() {
+        debug_assert_eq(11, slot_names.count());
+    }
+
+    fn check_noon() {
+        let test_data = tgm::load();
+        debug_assert_eq(test_data.name = "midday")
     }
 } // mod suite
 // ---- end of file ----
